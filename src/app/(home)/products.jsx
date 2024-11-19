f7daf6e3dcddb86fa14a74f20f1be0574e48c0b8
MohammadAlmohsen <muhammadalmohsen8@gmail.com>
2024-11-19 11:43:32 +0200

    lesson 9 Dealing with images(<Image/>)

--- src/app/(home)/products.jsx
+++ src/app/(home)/products.jsx
@@ -1,65 +1,71 @@
-import React from 'react';
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
-
+import React from "react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 const arr = [
-    { productIng: "./images/1.png" },
-    { productIng: "./images/2.webp" },
-    { productIng: "./images/3.webp" },
-    { productIng: "./images/4.webp" },
-    { productIng: "./images/5.webp" },
-    { productIng: "./images/6.webp" },
-    { productIng: "./images/7.webp" },
-    { productIng: "./images/8.png" },
-  ];
+  { productIng: "./images/1.png" },
+  { productIng: "./images/2.webp" },
+  { productIng: "./images/3.webp" },
+  { productIng: "./images/4.webp" },
+  { productIng: "./images/5.webp" },
+  { productIng: "./images/6.webp" },
+  { productIng: "./images/7.webp" },
+  { productIng: "./images/8.png" },
+];
 
-  // fetch data functhon on the server using fetch
-  async function getData() {
-   // await new Promise(resolve => setTimeout(resolve, 3000)) // Wait before executing the next code 
-    const res= await fetch("http://localhost:4000/products")
-    if(!res.ok){
-      notFound()
-    }
-    return res.json()
+// fetch data functhon on the server using fetch
+async function getData() {
+  // await new Promise(resolve => setTimeout(resolve, 3000)) // Wait before executing the next code
+  const res = await fetch("http://localhost:4000/products");
+  if (!res.ok) {
+    notFound();
   }
-const Products = async () => {
-  const arrData =await getData();
- 
-    return (
-        <section className="products flex">
-        {arrData.map((item) => {
-          return (
-            <article title={item.title} key={item.id} className="card">
-              <Link href={`/product-details/${item.id}`}>
-               <img width={266} src={item.productImg} alt=""/>
-              </Link>
-              <div style={{ width: "266px" }} className="content">
-                <h1 className="title">{item.title.slice(0,15)}...</h1>
-                <p className="description">
-                  {item.description.slice(0,111)}...
-                </p>
-                <div
-                  className="flex"
-                  style={{
-                    justifyContent: "space-between",
-                    paddingBottom: "0.7rem",
-                  }}
-                >
-                  <div className="price">${item.price}</div>
-                  <button className="add-to-cart flex">
-                   
-                    <FontAwesomeIcon style={{width:"1.1rem"}} icon={faCartPlus}/>
-                    Add To Cart
-                  </button>
-                </div>
-              </div>
-            </article>
-          );
-        })}
-      </section>    
-    );
+  return res.json();
 }
+const Products = async () => {
+  const arrData = await getData();
+
+  return (
+    <section className="products flex">
+      {arrData.map((item) => {
+        return (
+          <article title={item.title} key={item.id} className="card">
+            <Link href={`/product-details/${item.id}`}>
+              <Image
+                width={266}
+                height={260}
+                quality={100}
+                src={item.productImg}
+                alt=""
+              />
+            </Link>
+            <div style={{ width: "266px" }} className="content">
+              <h1 className="title">{item.title.slice(0, 15)}...</h1>
+              <p className="description">{item.description.slice(0, 111)}...</p>
+              <div
+                className="flex"
+                style={{
+                  justifyContent: "space-between",
+                  paddingBottom: "0.7rem",
+                }}
+              >
+                <div className="price">${item.price}</div>
+                <button className="add-to-cart flex">
+                  <FontAwesomeIcon
+                    style={{ width: "1.1rem" }}
+                    icon={faCartPlus}
+                  />
+                  Add To Cart
+                </button>
+              </div>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
 
 export default Products;
--- src/app/product-details/[id]/page.jsx
+++ src/app/product-details/[id]/page.jsx
@@ -5,6 +5,7 @@ import "./product-details.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 async function getData(iddd) {
   // await new Promise(resolve => setTimeout(resolve, 3000)) // Wait before executing the next code
   const res = await fetch(`http://localhost:4000/products/${iddd}`);
@@ -14,16 +15,13 @@ async function getData(iddd) {
   return res.json();
 }
 
-
-
-  export async function generateMetadata({ params }) {
-    const ObjData = await getData(params.id);
-    return {
-        title: ObjData.title,
-        description: ObjData.description,
-    }
-  }
-
+export async function generateMetadata({ params }) {
+  const ObjData = await getData(params.id);
+  return {
+    title: ObjData.title,
+    description: ObjData.description,
+  };
+}
 
 const Page = async ({ params }) => {
   const ObjData = await getData(params.id);
@@ -38,18 +36,22 @@ const Page = async ({ params }) => {
     >
       <Header />
       <main style={{ textAlign: "center" }} className="flex">
-        <img alt="" src={`.${ObjData.productImg}`}/>
+        <Image
+          width={260}
+          height={260}
+          quality={100}
+          alt=""
+          src={`${ObjData.productImg}`}
+        />
         <div className="product-details">
           <div style={{ justifyContent: "space-between" }} className="flex">
             <h2>{ObjData.title}</h2>
             <p className="price">${ObjData.price}</p>
           </div>
-          <p className="description">
-          {ObjData.description}
-          </p>
+          <p className="description">{ObjData.description}</p>
           <button className="flex add-to-cart">
             <i className="fa-solid fa-cart-plus" />
-            <FontAwesomeIcon style={{width:"1.1rem"}} icon={faCartPlus}/>
+            <FontAwesomeIcon style={{ width: "1.1rem" }} icon={faCartPlus} />
             Add To Cart
           </button>
         </div>
